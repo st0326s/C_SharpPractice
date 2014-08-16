53284f886b709fb6c882a85d874098ccd7381791
Satoshi Miyamoto <satoshidev.0326@gmail.com>
2014-08-16 14:36:00 +0900

    新規ファイルを追加し、Menu.csは変更

---
+++ Assets/Script/test1.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnEnable()
+    {
+        Debug.Log("aaa");
+    }
+}
